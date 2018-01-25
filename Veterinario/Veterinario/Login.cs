156f09343a6adf0347caf39939df45c65c9ff318
rojo5 <rojo5@PT-AZUL>
2018-01-25 11:53:30 +0100

    Clase form cambiada a Login, y creada la Clase contendor

--- Veterinario/Veterinario/Login.cs
+++ Veterinario/Veterinario/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Veterinario
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
             Console.WriteLine("PORG con torpedos sexualfdses");
